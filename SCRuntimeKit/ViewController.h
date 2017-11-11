978c703026624bbfe1ad251e69e70fb74d20bcd6
linqin <linqin@didichuxing.com>
2017-11-11 13:58:41 +0800

    Initial Commit

---
+++ SCRuntimeKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCRuntimeKit
+//
+//  Created by scorpiolin on 2017/11/11.
+//  Copyright © 2017年 scorpiolin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SCRuntimeKitTests/SCRuntimeKitTests.m
@@ -0,0 +1,39 @@
+//
+//  SCRuntimeKitTests.m
+//  SCRuntimeKitTests
+//
+//  Created by scorpiolin on 2017/11/11.
+//  Copyright © 2017年 scorpiolin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SCRuntimeKitTests : XCTestCase
+
+@end
+
+@implementation SCRuntimeKitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
